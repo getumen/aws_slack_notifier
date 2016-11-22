9957702e3d5d85abce48df35f4d54ee8d0351734
中里佳央 <n.yoshihiro.jp@gmail.com>
2016-11-22 16:55:14 +0900

    impl all

--- notify.py
+++ notify.py
@@ -15,20 +15,37 @@ if __name__ == '__main__':
     client = boto3.client(
         'cloudwatch',
         aws_access_key_id=AWS_KEY_ID,
-        aws_secret_access_key=AWS_SECRET_KEY
+        aws_secret_access_key=AWS_SECRET_KEY,
+        region_name=AWS_REGION,
     )
-    print(client.describe_alarm_history())
 
-    end = datetime.datetime.utcnow()
+    end = datetime.datetime.today()
     start = datetime.datetime(end.year, end.month, 1)
+    response = client.get_metric_statistics (
+        MetricName = 'EstimatedCharges',
+        Namespace  = 'AWS/Billing',
+        Period     = 60*60*24,
+        StartTime  = start,
+        EndTime    = end,
+        Statistics = ['Maximum'],
+        Dimensions = [
+            {
+                'Name': 'Currency',
+                'Value': 'USD'
+            }
+        ]
+    )
 
-    text = ''
+    maximum = response['Datapoints'][0]['Maximum']
+    date    = response['Datapoints'][0]['Timestamp'].strftime('%Y年%m月%d日')
+
+    text = "%sまでのAWSの料金は、$%sです。" % (date, maximum)
 
     params = {'token':SLACK_TOKEN,   # トークン
-             'channel':'admin', # チャンネルID
-             'text': 'bot'    # 送信するテキスト
+             'channel':'bossgakita', # チャンネルID
+             'text': text    # 送信するテキスト
     }
 
     params = urllib.parse.urlencode(params)
     headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-#    requests.get("https://slack.com/api/chat.postMessage", params=params, headers=headers)
+    requests.get("https://slack.com/api/chat.postMessage", params=params, headers=headers)
