9d8d566646e0150a9c971a849e39407dd41b571b
中里佳央 <n.yoshihiro.jp@gmail.com>
2016-11-22 16:10:21 +0900

    a

--- env.py
+++ env.py
@@ -1,6 +1,6 @@
 import os
 
-AWS_REGION = os.environ('AWS_REGION')
-AWS_KEY_ID = os.environ('AWS_KEY_ID')
-AWS_SECRET_KEY = os.environ('AWS_SECRET_KEY')
-SLACK_TOKEN = os.environ('SLACK_TOKEN')
+AWS_REGION = os.environ['AWS_REGION']
+AWS_KEY_ID = os.environ['AWS_KEY_ID']
+AWS_SECRET_KEY = os.environ['AWS_SECRET_KEY']
+SLACK_TOKEN = os.environ['SLACK_TOKEN']
