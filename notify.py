26b8bbea9f7e154a63b4453fd8acc3f1f85d3462
中里佳央 <n.yoshihiro.jp@gmail.com>
2016-11-22 15:57:37 +0900

    bug fix

--- notify.py
+++ notify.py
@@ -4,38 +4,41 @@ import env
 import requests
 import urllib
 
-conn = boto.ec2.cloudwatch.connect_to_region(
-    AWS_REGION,
-    aws_access_key_id=env.AWS_KEY_ID,
-    aws_secret_access_key=env.AWS_SECRET_KEY)
+if __name__ == '__main__':
 
-end = datetime.datetime.utcnow()
-start = datetime.datetime(end.year, end.month, 1)
-　
-services = conn.list_metrics()
+    conn = boto.ec2.cloudwatch.connect_to_region(
+        AWS_REGION,
+        aws_access_key_id=env.AWS_KEY_ID,
+        aws_secret_access_key=env.AWS_SECRET_KEY)
 
-text = ''
+    end = datetime.datetime.utcnow()
+    start = datetime.datetime(end.year, end.month, 1).astimezone(pytz.utc)
 
-for service in services:
-    value = conn.get_metric_statistics(
-        3600,
-        start,
-        end,
-        'EstimatedCharges',
-        'AWS/Billing',
-        'Maximum',
-        dimensions = {'ServiceName':[service], 'Currency':['USD']}
+    services = conn.list_metrics()
+
+    text = ''
+
+    for service in services:
+        value = conn.get_metric_statistics(
+            3600,
+            start,
+            end,
+            'EstimatedCharges',
+            'AWS/Billing',
+            'Maximum',
+            dimensions = {'ServiceName':[service], 'Currency':['USD']}
+            )
 
         if not value:
-                continue
+            continue
         text += service + ': '
         text += str(value) + '/'
         text +=  str(value[0]['Maximum']) + '\n'
 
-params = {'token':env.SLACK_TOKEN,   # トークン
-         'channel':'admin', # チャンネルID
-         'text': 'bot'    # 送信するテキスト
-}
-params = urllib.parse.urlencode(params)
-headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-requests.get("https://slack.com/api/chat.postMessage", params=params, headers=headers)
+    params = {'token':env.SLACK_TOKEN,   # トークン
+             'channel':'admin', # チャンネルID
+             'text': 'bot'    # 送信するテキスト
+    }
+    params = urllib.parse.urlencode(params)
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    requests.get("https://slack.com/api/chat.postMessage", params=params, headers=headers)
