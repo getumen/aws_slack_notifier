a9d5ca722b6999df5af55ab958a938ab2cf85ac6
yoshihiro <yoshihiro@mdl.cs.tsukuba.ac.jp>
2016-11-25 08:48:33 +0900

    a

--- notify.py
+++ notify.py
@@ -14,41 +14,42 @@ SLACK_CHANNEL = os.environ['SLACK_CHANNEL']
 
 
 if __name__ == '__main__':
-    client = boto3.client(
-        'cloudwatch',
-        aws_access_key_id=AWS_KEY_ID,
-        aws_secret_access_key=AWS_SECRET_KEY,
-        region_name=AWS_REGION,
-    )
+    while True:
+        client = boto3.client(
+            'cloudwatch',
+            aws_access_key_id=AWS_KEY_ID,
+            aws_secret_access_key=AWS_SECRET_KEY,
+            region_name=AWS_REGION,
+        )
 
-    end = datetime.datetime.now()
-    start = datetime.datetime(end.year, end.month, 1)
-    response = client.get_metric_statistics (
-        MetricName = 'EstimatedCharges',
-        Namespace  = 'AWS/Billing',
-        Period     = 60*60*24*7,
-        StartTime  = start,
-        EndTime    = end,
-        Statistics = ['Maximum'],
-        Dimensions = [
-            {
-                'Name': 'Currency',
-                'Value': 'USD'
-            }
-        ]
-    )
+        end = datetime.datetime.now()
+        start = datetime.datetime(end.year, end.month, 1)
+        response = client.get_metric_statistics (
+            MetricName = 'EstimatedCharges',
+            Namespace  = 'AWS/Billing',
+            Period     = 60*60*24*7,
+            StartTime  = start,
+            EndTime    = end,
+            Statistics = ['Maximum'],
+            Dimensions = [
+                {
+                    'Name': 'Currency',
+                    'Value': 'USD'
+                }
+            ]
+        )
 
-    maximum = response['Datapoints'][0]['Maximum']
-    date    = response['Datapoints'][0]['Timestamp'].strftime('%Y年%m月%d日')
+        maximum = response['Datapoints'][0]['Maximum']
+        date    = response['Datapoints'][0]['Timestamp'].strftime('%Y年%m月%d日')
 
-    text = "%sまでのAWSの料金は、$%sです。" % (date, maximum)
+        text = "%sまでのAWSの料金は、$%sです。" % (date, maximum)
 
-    params = {'token': SLACK_TOKEN,   # トークン
-             'channel': SLACK_CHANNEL, # チャンネルID
-             'text': text    # 送信するテキスト
-    }
+        params = {'token': SLACK_TOKEN,   # トークン
+                 'channel': SLACK_CHANNEL, # チャンネルID
+                 'text': text    # 送信するテキスト
+        }
 
-    params = urllib.parse.urlencode(params)
-    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-    requests.get("https://slack.com/api/chat.postMessage", params=params, headers=headers)
-    time.sleep(60*60*24)
+        params = urllib.parse.urlencode(params)
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        requests.get("https://slack.com/api/chat.postMessage", params=params, headers=headers)
+        time.sleep(60*60*24)
